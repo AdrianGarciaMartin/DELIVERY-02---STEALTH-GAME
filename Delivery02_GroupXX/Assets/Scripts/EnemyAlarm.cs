a8e0f28adc944f308bff3af797a5f9ca1bcfe64d
JLMCE <jmayhuacharallae@edu.tecnocampus.cat>
2024-02-11 19:57:21 +0100

    Enemy patrolling movement and enemy vision range and angle
    The conditions have yet to be correctly implemented.

--- Delivery02_GroupXX/Assets/Scripts/EnemyAlarm.cs
+++ Delivery02_GroupXX/Assets/Scripts/EnemyAlarm.cs
@@ -6,15 +6,64 @@ using UnityEngine;
 public class EnemyAlarm : MonoBehaviour
 {
     SpriteRenderer _alarmRenderer;
+    [SerializeField] PlayerMovement _PlayerMovement;
+    [SerializeField] Transform _DetectionPoint;
+    [SerializeField] float _VisionCone = 4f;
+    [SerializeField] float _DistanceForLettingGo = 5f;
 
-    public void PlayerDetected()
+    Transform _Target;
+
+    RaycastHit _RaycastHit = new RaycastHit();
+
+    void FixedUpdate() /* José Luis Mayhua-Charalla Espinoza, initializing the AI loop */
     {
-        ChangeColor(Color.red);
+        if (!_Target)
+        {
+            PlayerDetected();
+        }
+        else
+        {
+            PlayerLeft();
+        }
     }
 
-    public void PlayerLeft()
+    public void PlayerDetected() /* José Luis Mayhua-Charalla Espinoza, it launched a RaycastHit
+                                  * to detect the player, and if not, the AI does not move */
+    {
+        ChangeColor(Color.red);
+
+        if (Physics.Raycast(_DetectionPoint.position, _DetectionPoint.forward, out _RaycastHit, _VisionCone))
+        {
+            if (_RaycastHit.collider.CompareTag("Player"))
+            {
+                _Target = _RaycastHit.collider.transform;
+            }
+        }
+
+        /* "_PlayerMovement == false" */
+    }
+
+    public void PlayerLeft() /* José Luis Mayhua-Charalla Espinoza, lead the AI movement */
     {
         ChangeColor(new Color(0,0,0,0));
+
+        Vector2 direction = _Target.position - transform.position;
+        float distance = direction.magnitude;
+        if (distance > _DistanceForLettingGo)
+        {
+            _Target = null;
+        }
+        else
+        {
+            if (_Target.position.x < transform.position.x)
+            {
+                /* "_PlayerMovement == MoveLeft" */
+            }
+            else
+            {
+                /* "PlayerMovement == MoveRight" */
+            }
+        }
     }
 
     private void ChangeColor(Color color)
